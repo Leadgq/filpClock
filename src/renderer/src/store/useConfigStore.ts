603795446a619ab955e97ae3199f65e3594bf7af
gaoqiang <gaoqiang@yundun119.com>
2023-09-21 16:23:16 +0800

    修改

--- src/renderer/src/store/useConfigStore.ts
+++ src/renderer/src/store/useConfigStore.ts
@@ -1,5 +1,9 @@
 import { defineStore } from 'pinia'
 
+type footerType = {
+  bgColor?: string
+  color?: string
+}
 type configType = {
   type: 'clock' | 'timing'
   timing: {
@@ -8,24 +12,38 @@ type configType = {
     second?: number
   }
 }
+type ClockType = {
+  config: configType
+  footer: footerType
+}
 export const useConfigStore = defineStore('config', {
   state: () => {
     return {
-      config: <configType>{
-        type: 'clock' as 'clock' | 'timing',
-        timing: {
-          hour: 0,
-          minute: 8,
-          second: 0
+      clock: <ClockType>{
+        config: <configType>{
+          type: 'clock' as 'clock' | 'timing',
+          timing: {
+            hour: 0,
+            minute: 0,
+            second: 0
+          }
+        },
+        footer: {
+          bgColor: 'red',
+          color: '#fff'
         }
       }
     }
   },
   actions: {
-    setConfig(config: configType) {
-      this.config = {
-        ...this.config,
+    setConfig(config: configType, footer?: footerType) {
+      this.clock.config = {
+        ...this.clock.config,
         ...config
+      }
+      this.clock.footer = {
+        ...this.clock.footer,
+        ...footer
       }
     }
   }
--- src/renderer/src/views/Clock.vue
+++ src/renderer/src/views/Clock.vue
@@ -10,44 +10,30 @@ import FlipClock from '../composables/FlipClock'
 import Footer from '../components/Footer.vue'
 import { watch, ref, onMounted } from 'vue'
 import '@renderer/assets/flipclock.scss'
-import { storeToRefs } from 'pinia'
 import { useConfigStore } from '@renderer/store/useConfigStore'
 const store = useConfigStore()
-const { config } = storeToRefs(store)
-const { setConfig } = store
 const instance = ref<FlipClock>()
 
 onMounted(() => {
   instance.value = new FlipClock({
     el: '#flip-number',
-    type: config.value.type,
-    timing: config.value.timing
+    type: store.clock.config.type,
+    timing: store.clock.config.timing
   })
   instance.value.render()
 })
 
 watch(
-  () => config.value.type,
+  () => store.clock.config.type,
   () => {
     if (instance.value) {
       instance.value
         .destroy()
-        .config({ el: '#flip-number', ...config.value })
+        .config({ el: '#flip-number', ...store.clock.config })
         .render()
     }
   }
 )
-
-setTimeout(() => {
-  setConfig({
-    type: 'timing',
-    timing: {
-      hour: 0,
-      minute: 8,
-      second: 0
-    }
-  })
-}, 5000)
 </script>
 
 <style lang="scss">
